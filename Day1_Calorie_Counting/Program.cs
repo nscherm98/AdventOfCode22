386173f427169dc4009524513fc9388977913f53
Nicklas Scherm <n.scherm@schenckprocess.com>
2022-12-05 16:38:26 +0100

    Solution for part 1

--- Day1_Calorie_Counting/Program.cs
+++ Day1_Calorie_Counting/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class Solution
+{
+    static void Main(String[] args)
+    {
+        string path = "C:\\Users\\n.scherm\\source\\repos\\AdventOfCode22\\Day1_Calorie_Counting\\input.txt";
+        string[] lines = File.ReadAllLines(path);
+
+        var totalCalories = new List<int>();
+        int calories = 0;
+
+        foreach (var line in lines)
+        {
+            if (!String.IsNullOrWhiteSpace(line))
+            {
+                calories += int.Parse(line);
+                continue;
+            }
+            totalCalories.Add(calories);
+            calories = 0;
+        }
+        totalCalories.Add(calories);
+
+        // find maximum
+        var maxCalories = totalCalories.Max();
+        Console.WriteLine("Maximum: " + maxCalories);
+    }
+}
